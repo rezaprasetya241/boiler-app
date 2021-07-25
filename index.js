7f53780c15a495cffb0206dc2fb0f7f35a6e11ec
rezaagung <reza1900018404@webmail.uad.ac.id>
2021-07-26 00:08:30 +0700

    move private information into dev.js

--- index.js
+++ index.js
@@ -2,13 +2,41 @@
 const express = require('express');
 const app = express();
 
+// Use bodyParser
+const bodyParser = require('body-parser');
+
+// Use cookieParser
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key')
+
+// Use User
+const { User } = require('./models/user');
+
 // use mongoose and connect to mongoDb database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://rezaprasetya:reza123@boiler-app-shard-00-00.5wvjk.mongodb.net:27017,boiler-app-shard-00-01.5wvjk.mongodb.net:27017,boiler-app-shard-00-02.5wvjk.mongodb.net:27017/boiler?ssl=true&replicaSet=atlas-j0bq0o-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then( () => console.log('DB connected'))
+mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}).then( () => console.log('DB connected'))
                         .catch(err => console.error(err));
+
+// bring google parser use body and cookie parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req,res) =>{
+    const user = new User(req.body)
+
+    user.save((err, userData) =>{
+        if(err) {
+            return res.json({success: false, err})
+        }
+        return res.status(200).json({success: true})
+    })
+})
+
 // route handling
 app.get('/', (req,res) =>{
-    res.send('hello world')
+    res.json('hello reza agung prasetya')
 });
 
 
